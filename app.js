0aa05c5c2ee405855319356e103563cacc2099ed
ASTHAPATEL21 <153778147+ASTHAPATEL21@users.noreply.github.com>
2025-03-15 22:35:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ function checkAns(idx) {
       setTimeout(levelUp, 1000);
     }
   } else {
-    h2.innerHTML = `Game Over! Your score was <b>" ${level} "</b> <br> Press any key to start`;
+    h2.innerHTML = `Game Over! Your score was <b>" ${level - 1} "</b> <br> Press any key to start`;
     document.querySelector("body").style.backgroundColor = "red";
     setTimeout(function () {
       document.querySelector("body").style.backgroundColor = "white";
